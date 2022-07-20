3da6da852e80e126bf3531b604e814e4bbe0e5b9
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-07-20 09:32:46 -0500

    Add project files.

---
+++ MonitoringApi/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MonitoringApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+
+    // GET: api/<UsersController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        throw new Exception("Something bad happened here.");
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        try
+        {
+            if (id < 0 || id > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            _logger.LogInformation(@"The api\Users\{id} was called", id);
+            return Ok($"Value{id}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "This given Id of {Identifier} was invalid.", id);
+            return BadRequest("The index was out of range.");
+        }
+
+
+        /*        if(id < 0 || id > 100)
+                {
+                    _logger.LogWarning("The given Id of {Identifier} was invalid.", id);
+                    return BadRequest("The index was out of range.");
+                }
+                _logger.LogInformation(@"The api\Users\{id} was called", id);
+                return Ok($"Value{id}");*/
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
---
+++ MonitoringApi/Program.cs
@@ -0,0 +1,56 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MonitoringApi.HealthChecks;
+using WatchDog;
+
+////https://github.com/IzyPro/WatchDog
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site Health Check")
+    .AddCheck<RandomHealthCheck>("Database Health Check");
+builder.Services.AddWatchDogServices();
+
+builder.Services.AddHealthChecksUI(opts =>
+{
+    opts.AddHealthCheckEndpoint("api", "/health");
+    opts.SetEvaluationTimeInSeconds(5);
+    opts.SetMinimumSecondsBetweenFailureNotifications(10);
+}).AddInMemoryStorage();
+
+var app = builder.Build();
+
+app.UseWatchDogExceptionLogger();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecksUI();
+
+app.UseWatchDog(opts =>
+{
+    opts.WatchPageUsername = app.Configuration.GetValue<string>("WatchDog:UserName");
+    opts.WatchPagePassword = app.Configuration.GetValue<string>("WatchDog:Password");
+    opts.Blacklist = "health";
+});
+
+app.Run();
